fb64167e3751fd2a05cb6a7e611468bae7be68e6
An Dang <dangthanhan507@gmail.com>
2023-07-07 02:56:57 -0700

    Creating setup software to interface with hl2ss. Also tested RGBD, will move over to hl2ss_utils soon. TODO: add a main.py to begin server software

---
+++ hl2ss_stream.py
@@ -0,0 +1,164 @@
+#------------------------------------------------------------------------------
+# hl2ss_stream.py
+# 
+# This is a stream wrapper for the hl2ss code. Abstracts away all of the 
+# complexity involved with using the hl2ss code. This will be in the form of a 
+# class where method calls will give us what we need for the sensor data  
+# extracted from hl2ss. The details of the setup streaming code should not be 
+# too relevant in this repo, so we can hardcode them in this file.
+#------------------------------------------------------------------------------
+
+
+ROOT_PATH = "/home/andang/workspace/CV_Lab/Hololens2-CV-Server/"
+
+#library imports
+import multiprocessing as mp
+import numpy as np
+import cv2
+import os
+import sys
+sys.path.append(os.path.join(ROOT_PATH,'hl2ss','viewer'))
+#hl2ss specific imports
+import hl2ss
+import hl2ss_mp
+import hl2ss_3dcv
+
+# hl2ss Settings --------------------------------------------------------------
+HOST_IP = '192.168.43.19' #ipv4 of hololens
+
+#path to hololens calibration data (extrinsics/intrinsics)
+CALIB_PATH = os.path.join(ROOT_PATH,'hl2ss','calibration')
+
+# PV camera params
+PV_WIDTH = 640
+PV_HEIGHT = 360
+PV_FPS = 30
+PV_PROFILE = hl2ss.VideoProfile.H265_MAIN
+PV_BITRATE = hl2ss.get_video_codec_bitrate(PV_WIDTH, PV_HEIGHT, PV_FPS, hl2ss.get_video_codec_default_factor(PV_PROFILE))
+
+# General Params
+
+#### in seconds
+BUFFER_LENGTH = 10
+
+# Longthrow Constants
+MAX_DEPTH = 3.0
+
+#------------------------------------------------------------------------------
+class Hl2ssData:
+    def __init__(self, data_lt,data_pv, pv_intrinsics):
+        self.data_lt = data_lt
+        self.data_pv = data_pv
+        self.pv_intrinsics = pv_intrinsics
+
+
+'''
+Hl2ssStreamWrapper Features:
+    -> RGB stream
+    -> Depth stream
+    -> RGBD fusion
+
+    -> Obtain associated pose with data
+'''
+class Hl2ssStreamWrapper:
+    def __init__(self):
+        #setup and store Longthrow sensor calibrations
+        self.calib_lt = hl2ss_3dcv._load_calibration_rm(hl2ss.StreamPort.RM_DEPTH_LONGTHROW, CALIB_PATH)
+        uv2xy = hl2ss_3dcv.compute_uv2xy(self.calib_lt.intrinsics, hl2ss.Parameters_RM_DEPTH_LONGTHROW.WIDTH, hl2ss.Parameters_RM_DEPTH_LONGTHROW.HEIGHT)
+        self.xy1, self.lt_scale = hl2ss_3dcv.rm_depth_compute_rays(uv2xy, self.calib_lt.scale)
+
+        #setup pv calibrations (placeholder)
+        self.pv_extrinsics = np.eye(4, 4, dtype=np.float32)
+
+        # stream setup producer
+        self.producer = hl2ss_mp.producer()
+        self.consumer = hl2ss_mp.consumer()
+        self.manager = mp.Manager()
+
+        # sink placeholder variables
+        self.sink_pv = None
+        self.sink_depth = None
+
+        self.running = False
+    
+    def start(self):
+        '''
+            Start up the Hololens Streaming. Sets up all necessary producer/consumer
+            and sinks to achieve streaming capabilities
+        '''
+        hl2ss.start_subsystem_pv(HOST_IP, hl2ss.StreamPort.PERSONAL_VIDEO)
+
+        #configure profiles
+        self.producer.configure_pv(True, HOST_IP, hl2ss.StreamPort.PERSONAL_VIDEO, hl2ss.ChunkSize.PERSONAL_VIDEO, hl2ss.StreamMode.MODE_1, PV_WIDTH, PV_HEIGHT, PV_FPS, PV_PROFILE, PV_BITRATE, 'rgb24')
+        self.producer.configure_rm_depth_longthrow(True, HOST_IP, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.ChunkSize.RM_DEPTH_LONGTHROW, hl2ss.StreamMode.MODE_1, hl2ss.PngFilterMode.Paeth)
+
+        #initialize producer
+        self.producer.initialize(hl2ss.StreamPort.PERSONAL_VIDEO, PV_FPS * BUFFER_LENGTH)
+        self.producer.initialize(hl2ss.StreamPort.RM_DEPTH_LONGTHROW, hl2ss.Parameters_RM_DEPTH_LONGTHROW.FPS * BUFFER_LENGTH)
+
+        #start producer
+        self.producer.start(hl2ss.StreamPort.PERSONAL_VIDEO)
+        self.producer.start(hl2ss.StreamPort.RM_DEPTH_LONGTHROW)
+
+        #setup consumer
+        self.sink_pv = self.consumer.create_sink(self.producer, hl2ss.StreamPort.PERSONAL_VIDEO, self.manager, None)
+        self.sink_depth = self.consumer.create_sink(self.producer, hl2ss.StreamPort.RM_DEPTH_LONGTHROW, self.manager, ...)
+
+        self.sink_pv.get_attach_response()
+        self.sink_depth.get_attach_response()
+
+        self.running = True
+
+    def stop(self):
+        '''
+            Stops the Hololens Streaming. Must call this to ensure we can re-run program.
+            If not, resources will still be allocated on program exit on the Hololens side.
+        '''
+        #stop network streams
+        self.sink_pv.detach()
+        self.sink_depth.detach()
+        self.producer.stop(hl2ss.StreamPort.PERSONAL_VIDEO)
+        self.producer.stop(hl2ss.StreamPort.RM_DEPTH_LONGTHROW)
+        hl2ss.stop_subsystem_pv(HOST_IP, hl2ss.StreamPort.PERSONAL_VIDEO)
+
+        #reset variables
+        self.sink_pv = None
+        self.sink_depth = None
+
+        #update class members
+        self.running = False
+
+    def isRunning(self):
+        return self.running
+    
+
+    # TAKING DATA from stream
+    # ALWAYS CHECKS IF STREAM IS RUNNING
+
+    def waitReady(self):
+        if self.isRunning():
+            self.sink_depth.acquire()
+
+    def getData(self):
+        if self.isRunning():
+            #get depth and rgb data from appropriate sensors
+            #NOTE: we align rgb to longthrow instead of vice versa because longthrow is slower.
+            #so we can't get a best longthrow for a given rgb, but we can get a best rgb for a given longthrow
+            _, data_lt = self.sink_depth.get_most_recent_frame()
+            if ((data_lt is None) or (not hl2ss.is_valid_pose(data_lt.pose))):
+                return None
+
+            _, data_pv = self.sink_pv.get_nearest(data_lt.timestamp)
+            if ((data_pv is None) or (not hl2ss.is_valid_pose(data_pv.pose))):
+                return None
+            
+            #TODO: add more data on here as we think about more things
+
+            #PV uses autofocus (intrinsics change), so we must store intrinsics in our data packet
+            #save changes to extrinsics directly to member
+            pv_intrinsics = hl2ss.update_pv_intrinsics(pv_intrinsics, data_pv.payload.focal_length, data_pv.payload.principal_point)
+            color_intrinsics, self.pv_extrinsics = hl2ss_3dcv.pv_fix_calibration(pv_intrinsics, self.pv_extrinsics)
+
+            return Hl2ssData(data_lt, data_pv, color_intrinsics)
+        else:
+            return None
---
+++ hl2ss_utils.py
@@ -0,0 +1,17 @@
+#------------------------------------------------------------------------------
+# hl2ss_utils.py
+# 
+# This file is meant to use hl2ss data to perform calculations that are
+# listed under pre-processing for hl2ss. For example, creating rgbd
+# is something we perform as a pre-process step after getting our data.
+#------------------------------------------------------------------------------
+
+import hl2ss_stream
+import os
+import sys
+sys.path.append(os.path.join(hl2ss_stream.ROOT_PATH,'hl2ss','viewer'))
+import hl2ss_3dcv
+
+def make_rgbd():
+    pass
+
